7fba317a27fb1a6e0d8a5ff37a8b066b5635a5cb
Hanna Borodina <venera131916@gmail.com>
2024-01-18 00:53:13 +0200

    add log

---
+++ Logger/Logger.cs
@@ -0,0 +1,57 @@
+﻿
+ public class Logger
+    {
+    protected readonly string logFilePath;
+    protected bool isRunning;
+
+    public Logger(string filePath)
+    {
+        logFilePath = filePath;
+        isRunning = true;
+    }
+
+    public virtual void Start(string loggerName)
+    {
+        Console.WriteLine($"{loggerName} started. Logging to {logFilePath}");
+    while(isRunning)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+            string logMessage = $"{timestamp} - {loggerName} is on!\n ";
+
+            File.AppendAllText(logFilePath, logMessage );
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    public void Stop()
+    {
+        isRunning = false;
+        Console.WriteLine("\nLogger stoped.\n");
+    }
+    }
+
+namespace LoggerNamespace1
+{
+    public class Logger1: Logger
+    {
+       static private readonly string filePath = "..\\..\\..\\default_log.txt";
+        public Logger1() : base(filePath) { }    
+        public override void Start(string loggerName)
+        {
+            base.Start($"Logger1 - {loggerName}");
+        }
+    }
+}
+
+namespace LoggerNamespace2
+{
+    public class Logger2 : Logger
+    {
+        public Logger2(string customFilePath) : base(customFilePath) { }
+        public override void Start(string loggerName)
+            {
+            base.Start($"Logger2 - {loggerName}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Создать класс Logger с методом Start() (пишет в файл каждую секундку своей жизни) до тех пор пока создан его экземпляр
+
+Предусмотреть ввод пользователем пути сохранения лог-файла / Предоставить удобное меню с выбором вариантов по пути (или стандартный или пользовательский)
+при старте ПО
+
+Разбить проект на 2 логгера в отдельных пространствах имен ( по пути)
+ */
+
+using  LoggerNamespace1;
+using  LoggerNamespace2;
+
+
+LoggerNamespace1.Logger1 sysLog = null;
+Thread sysThread = null;
+LoggerNamespace2.Logger2 customLog = null;
+Thread customThread = null;
+
+
+Console.WriteLine("Select log file path option -> ");
+Console.WriteLine("1. Use default path");
+Console.WriteLine("2. Enter custom path");
+
+string choice  = Console.ReadLine();
+string logFilePath;
+
+
+if (choice == "1")
+{
+    sysLog = new Logger1();
+    sysThread = new Thread(() => sysLog.Start("Thread 1"));
+    sysThread.Start();
+}
+else if (choice == "2")
+{
+    Console.WriteLine("Enter custom log file path ->");
+    logFilePath = Console.ReadLine();
+    customLog = new Logger2(logFilePath);
+    customThread = new Thread(() => customLog.Start("Thread 2"));
+    customThread.Start();
+}
+else
+{
+    Console.Write("Error!");
+}
+
+Console.WriteLine("Prress any key to stop logger");
+Console.ReadKey();
+
+customLog?.Stop();
+sysLog?.Stop();
+customThread?.Join();
+sysThread?.Join();
